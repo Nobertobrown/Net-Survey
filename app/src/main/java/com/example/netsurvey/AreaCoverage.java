c5b5cf499d1641d555098052b351b1d161ab6399
Nobertobrown <kibonanorberth@gmail.com>
2023-05-16 02:03:42 +0300

    add coverage page

--- app/src/main/java/com/example/netsurvey/AreaCoverage.java
+++ app/src/main/java/com/example/netsurvey/AreaCoverage.java
@@ -3,6 +3,8 @@ package com.example.netsurvey;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AreaCoverage extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class AreaCoverage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_area_coverage);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.links, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
